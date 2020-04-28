e5dcb859d4889b808816baed6a57baa48cb419c0
shenxf1986 <185693240@qq.com>
2020-04-28 15:58:47 +0800

    update

---
+++ myapp/src/layouts/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './index.less';
+import Link from 'umi/link';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+function BasicLayout(props) {
+    return (
+        <Layout>
+            <Header>
+                <div className={styles.logo}>王者荣耀资料库 </div>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['1']}
+                    style={{ lineHeight: '64px' }}
+                >
+                    <Menu.Item key="1">
+                        <Link to="hero">英雄</Link>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Link to="item">局内道具</Link>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Link to="summoner">召唤师技能</Link>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                    {props.children}
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Umi 入门教程 Created by xiaohuoni</Footer>
+        </Layout>
+    );
+}
+
+export default BasicLayout;
